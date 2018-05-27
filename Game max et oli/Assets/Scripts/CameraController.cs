54bfbfdcd7128a2259dd82df184186dc489e7ba9
oli5667 <39636440+oli5667@users.noreply.github.com>
2018-05-26 22:41:21 -0400

    camera added

---
+++ Game max et oli/Assets/Scripts/CameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 playerPosition;
+    public float cameraMoveSpeed;
+	
+	void Start ()
+    {
+		
+	}
+	
+	
+
+	void Update ()
+    {
+        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, -1);
+
+        transform.position = Vector3.Lerp(transform.position, playerPosition, cameraMoveSpeed * Time.deltaTime);
+    }
+
+}
