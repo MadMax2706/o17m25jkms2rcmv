2b9e159bbc01bf3db68badfa61fe32fb9eee3103
oli5667 <39636440+oli5667@users.noreply.github.com>
2018-05-27 16:08:49 -0400

    appuyer sur Q pour attaquer

---
+++ Game max et oli/Assets/Scripts/PlayerMeleeAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMeleeAttack : MonoBehaviour
+{
+    public bool attacking;
+    public float attackTime;
+    private float attackTimeCounter;
+    private Animator anim;
+
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+	}
+	
+	
+
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Q))
+        {
+            attackTimeCounter = attackTime;
+            attacking = true;
+
+            anim.SetBool("MeleeAttack", true);
+        }
+
+        if (attackTime > 0)
+        {
+            attackTimeCounter -= Time.deltaTime;
+        }
+
+        if (attackTimeCounter <= 0)
+        {
+            attacking = false;
+            anim.SetBool("MeleeAttack", false);
+        }
+
+    }
+
+
+}
