c601f8412c693e5748df71abb416dc941560118e
oli5667 <39636440+oli5667@users.noreply.github.com>
2018-05-29 20:37:12 -0400

    inventaire

---
+++ Game max et oli/Assets/Scripts/Inventory/BagScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BagScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject slotPrefab;
+    private CanvasGroup canvasGroup;
+    public bool IsOpen
+    {
+        get
+        {
+            return canvasGroup.alpha > 0;
+        }
+
+    }
+
+
+private void Awake()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+
+
+    public void AddSlots(int slotCount)
+    {
+        for (int i = 0; i < slotCount; i++)
+        {
+            Instantiate(slotPrefab, transform);
+        }
+
+    }
+
+
+
+    public void OpenClose()
+    {
+        canvasGroup.alpha = canvasGroup.alpha > 0 ? 0 : 1;
+
+        canvasGroup.blocksRaycasts = canvasGroup.blocksRaycasts == true ? false : true;
+    }
+
+
+}
---
+++ Game max et oli/Assets/Scripts/Inventory/InventoryScript.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+    private static InventoryScript instance;
+    public static InventoryScript MyInstance
+    {
+
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<InventoryScript>();
+            }
+
+            return instance;
+        }
+
+    }
+    [SerializeField]
+    private Item[] items;
+    [SerializeField]
+    private BagButton[] bagButtons;
+    private List<Bag> bags = new List<Bag>();
+    public bool CanAddBag
+    {
+        get
+        { return bags.Count < 3; }
+    }
+
+private void Awake()
+    {
+        Bag bag = (Bag)Instantiate(items[0]);
+        bag.Initialize(20);
+        bag.Use();
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            Bag bag = (Bag)Instantiate(items[0]);
+            bag.Initialize(20);
+            bag.Use();
+        }
+
+    }
+
+
+
+    public void AddBag(Bag bag)
+    {
+        foreach (BagButton bagButton in bagButtons)
+        {
+            if (bagButton.MyBag == null)
+            {
+                bagButton.MyBag = bag;
+                bags.Add(bag);
+                break;
+            }
+        }
+
+    }
+
+
+
+    public void OpenClose()
+    {
+        bool closedBag = bags.Find(x => !x.MyBagScript.IsOpen);
+
+        foreach (Bag bag in bags)
+        {
+            if (bag.MyBagScript.IsOpen != closedBag)
+            {
+                bag.MyBagScript.OpenClose();
+            }
+        }
+
+    }
+
+
+}
